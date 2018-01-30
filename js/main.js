4448e18e566dfe42e8b70d2ab07ab5cc56076fbb
noname-username <32517547+noname-username@users.noreply.github.com>
2018-01-30 08:14:41 +0300

    review final version

--- js/main.js
+++ js/main.js
@@ -1,13 +1,44 @@
 $(document).ready(function(){
 
-  $('.sliderHome').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    dots: true
-  });
+	$('.sliderHome').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		dots: true
+	});
 
+	$('.videoReview__previewSlider').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		fade: true,
+		draggable: false,
+		swipe: false,
+		arrows: false
+	});
 
+	$('.videoReview__TopRowSlider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		infinite: true,
+		focusOnSelect: true,
+		draggable: false,
+		asNavFor: '.videoReview__previewSlider',
+		nextArrow: '<div class="rowSlider__nextArr"></div>',
+		prevArrow: '<div class="hidden"></div>'
+
+	});
+
+	$('.videoReview__BottomRowSlider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		infinite: true,
+		focusOnSelect: true,
+		draggable: false,
+		asNavFor: '.videoReview__previewSlider',
+		prevArrow: '<div class="rowSlider__prevArr"></div>',
+		nextArrow: '<div class="hidden"></div>'
+	});
 
 });
