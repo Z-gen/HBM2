e0ef252754d4f88dff943c6c61a58be2b0b2c1ef
Z-gen <32515979+Z-gen@users.noreply.github.com>
2018-01-30 20:42:11 +0400

    Home changes

--- js/main.js
+++ js/main.js
@@ -8,6 +8,31 @@ $(document).ready(function(){
 		dots: true
 	});
 
+<<<<<<< HEAD
+	$('.sliderSteps').slick({
+		infinite: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		dots: false,
+		fade: true,
+		asNavFor: '.sliderStepImgSlider',
+		nextArrow: '<div class="stepSlider__nextArr"></div>',
+		prevArrow: '<div class="stepSlider__prevArr"></div>'
+	});
+
+	$('.sliderStepImgSlider').slick({
+		infinite: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		dots: false,
+		fade: true,
+		asNavFor: '.sliderSteps'
+	});
+
+
+
 	$('.videoReview__previewSlider').slick({
 		infinite: true,
 		slidesToShow: 1,
@@ -18,6 +43,18 @@ $(document).ready(function(){
 		arrows: false
 	});
 
+=======
+	$('.videoReview__previewSlider').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		fade: true,
+		draggable: false,
+		swipe: false,
+		arrows: false
+	});
+
+>>>>>>> 4f0106b4da71824357ac5340ba94fdf1341321aa
 	$('.videoReview__TopRowSlider').slick({
 		slidesToShow: 3,
 		slidesToScroll: 1,
@@ -40,5 +77,34 @@ $(document).ready(function(){
 		prevArrow: '<div class="rowSlider__prevArr"></div>',
 		nextArrow: '<div class="hidden"></div>'
 	});
+<<<<<<< HEAD
+
+
+	$(".sliderSteps .slick-arrow").click(function(){
+		if($("#sliderStep__item_1").hasClass("slick-current")) {
+			$(".stepNumbers ul li").removeClass("active");
+			$(".stepNumbers__1").addClass("active");
+		}
+		if($("#sliderStep__item_2").hasClass("slick-current")) {
+			$(".stepNumbers ul li").removeClass("active");
+			$(".stepNumbers__1").addClass("active");
+			$(".stepNumbers__2").addClass("active");
+		}
+		if($("#sliderStep__item_3").hasClass("slick-current")) {
+			$(".stepNumbers ul li").removeClass("active");
+			$(".stepNumbers__1").addClass("active");
+			$(".stepNumbers__2").addClass("active");
+			$(".stepNumbers__3").addClass("active");
+		}
+		if($("#sliderStep__item_4").hasClass("slick-current")) {
+			$(".stepNumbers__1").addClass("active");
+			$(".stepNumbers__2").addClass("active");
+			$(".stepNumbers__3").addClass("active");
+			$(".stepNumbers__4").addClass("active");
+		}
+		
+	});
+=======
+>>>>>>> 4f0106b4da71824357ac5340ba94fdf1341321aa
 
 });
